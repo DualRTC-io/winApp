ab0d7ad5a177f178fe735a0ea795b53b43811857
SungjungKim <ajax0615@gmail.com>
2015-12-02 04:04:19 +0900

    Add automatic open browser

--- src/windowControl/Form1.cs
+++ src/windowControl/Form1.cs
@@ -13,6 +13,8 @@ namespace windowControl
     public partial class Form1 : Form
     {
         private Boolean isRun = false;
+        private Boolean isChrome = true;
+        private Boolean isFirefox = true;
         public Form1()
         {
             InitializeComponent();
@@ -39,6 +41,37 @@ namespace windowControl
             }
             else
             {
+                System.Diagnostics.Process IEProcess = new System.Diagnostics.Process();
+                IEProcess.StartInfo.FileName = "chrome.exe";
+                IEProcess.StartInfo.Arguments = "https://112.108.40.152";
+                try 
+                {
+                    IEProcess.Start();
+                }
+                catch
+                {
+                    isChrome = false;
+                }
+
+                if (!isChrome)
+                {
+                    IEProcess.StartInfo.FileName = "firefox.exe";
+                    IEProcess.StartInfo.Arguments = "https://112.108.40.152";
+                    try
+                    {
+                        IEProcess.Start();
+                    }
+                    catch
+                    {
+                        isFirefox = false;
+                    }
+                }
+                if (!isChrome && !isFirefox)
+                {
+                    MessageBox.Show("크롬과 파이어폭스가 없어염.ㅠㅠ");
+                }
+            
+                
                 oNOFFToolStripMenuItem.Text = "ON(켜짐)";
                 isRun = !isRun;
             }
